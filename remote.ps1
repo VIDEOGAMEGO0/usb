3aebef28e3694191b6cab9824e0373d2e334b6ef
VIDEOGAMEGO0 <165396361+VIDEOGAMEGO0@users.noreply.github.com>
2025-08-05 15:19:07 +0200

    remote.ps1

--- remote.ps1
+++ remote.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile "$env:TEMP\ngrok.zip"
 Expand-Archive "$env:TEMP\ngrok.zip" -DestinationPath "$env:TEMP\ngrok"
-& "$env:TEMP\ngrok\ngrok.exe" authtoken TUO_TOKEN
+& "$env:TEMP\ngrok\ngrok.exe" authtoken cr_30pHX01REyhIa2l60hbaKG7rOLU
 & "$env:TEMP\ngrok\ngrok.exe" tcp 3389
