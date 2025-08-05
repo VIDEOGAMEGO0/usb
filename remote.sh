8715965374e32ebfc93bf83974861781564009f2
VIDEOGAMEGO0 <165396361+VIDEOGAMEGO0@users.noreply.github.com>
2025-08-05 15:40:16 +0200

    remote.sh

--- remote.sh
+++ remote.sh
@@ -1,5 +1,5 @@
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
 echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
 sudo apt update && sudo apt install ngrok
-ngrok config add-authtoken cr_30pHX01REyhIa2l60hbaKG7rOLU
+ngrok config add-authtoken 30pHX01REyhIa2l60hbaKG7rOLU_4RkBpq5Etq31CKdQB9gAB
 ngrok tcp 22  # O la porta che vuoi inoltrare
